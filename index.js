c83441f44942f7d8fe6574a73ee13b6015e8c032
Jamen Marzonie <jamenmarz@gmail.com>
2017-01-24 08:05:36 -0600

    Add patch to inconsistent file.dirname

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 var fs = require('fs')
 var mkdirp = require('mkdirp')
 var eventcb = require('event-callback')
+var dirname = require('path').dirname
 
 module.exports = write
 
 function write (file, callback) {
+  var filepath = file.path
+
   // Create necessary directories for file
-  mkdirp(file.dirname, function (err) {
+  mkdirp(dirname(filepath), function (err) {
     if (err) return callback(err)
 
     // Write contents to file system
     if (file.isStream()) {
-      var writer = file.contents.pipe(fs.createWriteStream(file.path))
+      var writer = file.contents.pipe(fs.createWriteStream(filepath))
       eventcb(writer, 'finish', callback)
     } else {
-      fs.writeFile(file.path, file.contents, callback)
+      fs.writeFile(filepath, file.contents, callback)
     }
   })
 }
